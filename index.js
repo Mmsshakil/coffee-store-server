dece665ff710ea30a7f6374f6215890e322f9663
Mmsshakil <softhandshakil@gmail.com>
2023-10-18 05:34:06 +0600

    update  coffee's all data done

--- index.js
+++ index.js
@@ -34,9 +34,9 @@ async function run() {
         await client.connect();
 
         const coffeeCollection = client.db("coffeeDB").collection('coffee');
-        
+
         // here we will read the newcoffee "R"
-        app.get('/coffee', async(req, res) =>{
+        app.get('/coffee', async (req, res) => {
             const cursor = coffeeCollection.find();
             const result = await cursor.toArray();
             res.send(result);
@@ -51,14 +51,46 @@ async function run() {
         })
 
         // here we will delete coffee "D"
-        app.delete('/coffee/:id', async(req, res) =>{
+        app.delete('/coffee/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: new ObjectId(id)}
+            const query = { _id: new ObjectId(id) };
             const result = await coffeeCollection.deleteOne(query);
             res.send(result);
 
         })
 
+
+        // here we will update coffee "U"
+        // -----------------------------------------------------------
+        app.get('/coffee/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await coffeeCollection.findOne(query);
+            res.send(result);
+
+        })
+
+        app.put('/coffee/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const options = { upsert: true };
+            const updatedCoffee = req.body;
+
+            const coffee = {
+                $set: {
+                    name: updatedCoffee.name,
+                    quantity: updatedCoffee.quantity,
+                    supplier: updatedCoffee.supplier,
+                    taste: updatedCoffee.taste,
+                    category: updatedCoffee.category,
+                    details: updatedCoffee.details,
+                    photo: updatedCoffee.photo
+                }
+            };
+            const result = await coffeeCollection.updateOne(filter, coffee, options);
+            res.send(result);
+        })
+        // --------------------------------------------------------------
 
 
 
